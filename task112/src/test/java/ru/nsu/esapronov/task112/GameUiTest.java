e5f452c14ea0215a9ba8cafbdc55484f793c6bce
Evgenii Sapronov <e.sapronov@g.nsu.ru>
2026-09-24 12:27:04 +0700

    javadoc

--- task112/src/test/java/ru/nsu/esapronov/task112/GameUiTest.java
+++ task112/src/test/java/ru/nsu/esapronov/task112/GameUiTest.java
@@ -4,17 +4,17 @@ import org.junit.jupiter.api.Test;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GameUITest {
+class GameUiTest {
     @Test
     void testAskPlayerChoice() {
-        GameUI ui = new GameUI(new Scanner("1\n"));
+        GameUi ui = new GameUi(new Scanner("1\n"));
 
         assertEquals("1", ui.askPlayerChoice());
     }
 
     @Test
     void testAskNextRound() {
-        GameUI ui = new GameUI(new Scanner("q\n"));
+        GameUi ui = new GameUi(new Scanner("q\n"));
 
         assertEquals("q", ui.askNextRound());
     }
